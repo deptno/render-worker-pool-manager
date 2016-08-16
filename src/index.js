c8188d3a14903f65db0e6eec616ed2ad4c13f997
bglee <deptno@gmail.com>
2016-08-17 01:09:34 +0900

    initial commit

---
+++ src/index.js
@@ -0,0 +1,86 @@
+import worker from 'phantomjs-render-worker';
+import {EventEmitter} from 'events';
+
+export default class WorkerCoach extends EventEmitter {
+    static event = {
+        init: 'init',
+        rendered: 'rendered',
+        error: 'error'
+    };
+
+    workers = [];
+    workState = {};
+    works = [];
+    initCount = 0;
+
+    constructor(poolSize, poller) {
+        super();
+        const {init, rendering, rendered, error} = worker.event;
+        for (let i = 0, w ; i < poolSize; i++) {
+            w = new worker('pdf', null, poller || this.poller);
+            w.on(init, this.onInit.bind(this, i));
+            w.on(rendering, this.onRendering.bind(this, i));
+            w.on(rendered, this.onRendered.bind(this, i));
+            w.on(error, this.onError.bind(this, i));
+            this.workers.push(w);
+        }
+    }
+    poller() {
+        // do override
+        return true;
+    }
+    setWorkState(workerId, state) {
+        this.workState[workerId] = state;
+    }
+    onInit(workerId) {
+        this.setWorkState(workerId, false);
+        if (++this.initCount === this.workers.length) {
+            console.log(`onInit$`);
+            this.emit(WorkerCoach.event.init);
+        }
+    }
+    onRendering(workerId) {
+        this.setWorkState(workerId, true);
+    }
+    onRendered(workerId, file) {
+        console.log(`onRendered ${workerId}`);
+        this.setWorkState(workerId, false);
+        this.emit(WorkerCoach.event.rendered, file);
+        this.run();
+    }
+    onError(workerId) {
+        console.log(`onError ${workerId}`);
+        this.setWorkState(workerId, false);
+        this.emit(WorkerCoach.event.rendered, arguments.slice(1));
+        this.run();
+    }
+
+    getWorker(workerId) {
+        return this.workers[workerId];
+    }
+    getIdleWorkerId() {
+        return Object.keys(this.workState).filter(id => !this.workState[id])[0];
+    }
+
+    push(url, file) {
+        this.works.push({ url, file });
+        this.run();
+        return file;
+    }
+
+    run() {
+        const workerId = this.getIdleWorkerId();
+        if (typeof workerId !== 'string') {
+            return;
+        }
+
+        const work = this.works.shift();
+        if (work) {
+            const worker = this.getWorker(workerId);
+            this.setWorkState(workerId, true);
+            console.log('render');
+            worker.render(work.url, work.file);
+            console.log(work.url, work.file);
+        }
+    }
+}
---
+++ test/phantomjs-render-worker-spec.js
@@ -0,0 +1,25 @@
+import Coach from '../built';
+
+describe('coach', () => {
+    const urls = [ 'http://google.com', 'http://naver.com' ];
+    const {init, rendered, error} = Coach.event;
+
+    it(`worker ${2}`, done => {
+        const coach = new Coach(2);
+        let renderCount = 0;
+
+        coach.on(init, () => {
+            const files = urls.map((url, index) => {
+                coach.push(url, `pdf/${index}.pdf`);
+            });
+            coach.on(rendered, (filename) => {
+                console.log(`${rendered}: ${filename}`);
+                if (++renderCount === urls.length) done();
+            });
+            coach.on(error, (filename) => {
+                console.log(`${error}: ${filename}`);
+                if (++renderCount === urls.length) done();
+            });
+        })
+    });
+});
